a29e64930ae88e2e16e4c438d6d73d2f45adde0d
Tim Fischer <tim.fischer@fh-erfurt.de>
2018-11-26 23:33:22 +0100

    replace setInterval with cron job

--- src/core/app-updater.js
+++ src/core/app-updater.js
@@ -1,21 +1,34 @@
 const releaseManager = require('./release-manager');
 const userState = require('./storage/user-state');
 const serverService = require('./network/server-service');
+const CronJob = require('cron').CronJob;
 
 class AppUpdater {
 
-    constructor() {}
+    constructor() {
+        this.job = new CronJob('0 */10 * * * *', this.onTick, this.onComplete);
+    }
+
+    onTick() {
+        const serverId = userState.currentServerId;
+        if (serverId !== null && serverId !== undefined) {
+            const server = serverService.getServerById(serverId);
+            serverService.getAppVersion(server.url).then((releaseVersion) => {
+                releaseManager.loadRelease(releaseVersion);
+            }, error => console.error(error));
+        }
+    }
+
+    onComplete() {
+        console.log('job complete');
+    }
 
     checkForUpdates() {
-        setInterval(() => {
-            const serverId = userState.currentServerId;
-            if (serverId !== null && serverId !== undefined) {
-                const server = serverService.getServerById(serverId);
-                serverService.getAppVersion(server.url).then((releaseVersion) => {
-                    releaseManager.loadRelease(releaseVersion);
-                }, error => console.error(error));
-            }
-        }, 3000);
+        this.job.start();
+    }
+
+    stop() {
+        this.job.stop();
     }
 
 }
